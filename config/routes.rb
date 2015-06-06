4c023251696042feb251cc6efafef6b576428d84
Paul Swartz <pswartz@zipcar.com>
2015-06-06 16:49:27 -0400

    basic contact info

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :contact
   resources :events do
     collection do
       get :show
