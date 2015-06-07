4da4e2b777c8701e985d7934ed4a6571ddd292d2
Paul Swartz <pswartz@zipcar.com>
2015-06-07 10:46:37 -0400

    fancier names

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   validates :name, uniqueness: true, length: {minimum: 1, maximum: 99}
+
+  def fancy_name
+    name.split('_').map(&:capitalize).join(' ')
+  end
 end
