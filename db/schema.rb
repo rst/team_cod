46af2c1b62fe4eede04ab811fa5792395498348f
Alex Sghia-Hughes <asghiah@gmail.com>
2015-07-07 20:05:19 -0400

    change destroy to delete

--- db/schema.rb
+++ db/schema.rb
@@ -44,9 +44,6 @@ ActiveRecord::Schema.define(version: 20150702212654) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "interests", ["topic_id"], name: "index_interests_on_topic_id", using: :btree
-  add_index "interests", ["user_id"], name: "index_interests_on_user_id", using: :btree
-
   create_table "topics", force: :cascade do |t|
     t.string   "name",       limit: 100
     t.datetime "created_at",             null: false
