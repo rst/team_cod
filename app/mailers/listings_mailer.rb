a27e314f6ea198d5ac6110894db5bd0d8ad7fbec
Robert Thau <rst@ai.mit.edu>
2015-08-20 18:09:54 -0400

    Code to generate emails for job listings.

--- app/mailers/listings_mailer.rb
+++ app/mailers/listings_mailer.rb
@@ -1,2 +1,21 @@
 class ListingsMailer < ApplicationMailer
+
+  def user_weekly_email(user)
+    @user = user
+    @listings = 
+       user.events_of_interest
+           .where_current
+           .order("created_at desc")
+           .includes(:topics)
+    
+    mail(to: @user.email, subject: 'Job listings from YouthHub')
+  end
+
+  def self.weekly_send_job
+    users = User.includes(:topics)
+    users.each do |user|
+      self.user_weekly_email(user).deliver_now
+    end
+  end
+
 end
--- test/mailers/listings_mailer_test.rb
+++ test/mailers/listings_mailer_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ListingsMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_user_weekly_email_empty
+    u = users(:lucy)
+    assert u.events_of_interest.empty?
+    mail = ListingsMailer.user_weekly_email(u).deliver_now
+
+    assert_not ActionMailer::Base.deliveries.empty?
+    assert_match /didn\'t find any this week/, mail.body.to_s
+  end
+
+  def test_user_weekly_email_nonempty
+    u = users(:lucy)
+    cyberj = events(:cyber_jamboree)
+    programming = topics(:programming)
+
+    Interest.create! user: u, topic: programming
+    EventTopic.create! event: cyberj, topic: programming
+
+    assert_not u.events_of_interest.empty?
+
+    mail = ListingsMailer.user_weekly_email(u).deliver_now
+
+    assert_not ActionMailer::Base.deliveries.empty?
+    assert_match /#{Regexp.quote(cyberj.name)}/, mail.body.to_s
+  end
+
 end
