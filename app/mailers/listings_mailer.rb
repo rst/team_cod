554729db3e9568c8743efa5dddedee765b4cc3e6
Robert Thau <rst@ai.mit.edu>
2015-08-20 17:28:51 -0400

    Mailer, straight out of the generator.

---
+++ app/mailers/listings_mailer.rb
@@ -0,0 +1,2 @@
+class ListingsMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/listings_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/listings_mailer
+class ListingsMailerPreview < ActionMailer::Preview
+
+end
