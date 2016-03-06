afeccbd43c9cbf96a150cd5121ea669d7989e93b
Robert Thau <rst@ai.mit.edu>
2016-03-06 12:37:14 -0500

    Minor data cleanup: translate std XML entities before DB insert.

--- jobcase/jobload.rb
+++ jobcase/jobload.rb
@@ -11,21 +11,31 @@ JobCat.load_from_files
 
 jobdoc = File.open("jobcase/youthhub.xml") { |f| Nokogiri::XML(f) }
 
+def self.de_entitize(txt)
+  txt = txt.to_s
+  txt = txt.gsub('&quot;', '"')
+  txt = txt.gsub('&apos;', "'")
+  txt = txt.gsub('&gt;',   '>')
+  txt = txt.gsub('&lt;',   '<')
+  txt = txt.gsub('&amp;',  '&')
+  txt
+end
+
 jobdoc.css("job").each do |job|
   title = job.css("title").text
   guid = job.css("guid").text
   category = job.css("job-category").text.strip
-  url = job.css("url")
+  url = job.css("url").text
   zip_code = job.css("zipcode").text.strip
   jobcat = JobCat.data_for_category(category)
-  description = job.css("description")
+  description = job.css("description").text
   if boston_zip_codes.include?(zip_code) && jobcat && jobcat.training_lvl && jobcat.training_lvl <= 2
     ev = Event.find_or_initialize_by jobcase_id: guid
     ev.attributes = {
-      name: title,
-      description: description,
-      address: zip_code,
-      url: url
+      name: de_entitize(title),
+      description: de_entitize(description),
+      address: de_entitize(zip_code),
+      url: de_entitize(url)
     }
     if !ev.save
       puts "Could not save jobcase job #{guid}"
