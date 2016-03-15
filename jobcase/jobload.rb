41b97f7c981f8e49a830d6c3d643deaf6f2b8be1
Robert Thau <rst@ai.mit.edu>
2016-03-14 21:43:05 -0400

    Update Jobcase data-load script to deal with expiration.
    Any pre-existing event not in the new feed is marked expired immediately.
    Others are set to auto-expire, provisionally, in two weeks.

--- jobcase/jobload.rb
+++ jobcase/jobload.rb
@@ -9,7 +9,16 @@ boston_zip_codes = %w(
 
 JobCat.load_from_files
 
-jobdoc = File.open("jobcase/youthhub.xml") { |f| Nokogiri::XML(f) }
+filename = ARGV[0] || Date.today.strftime('jobcase/yh_%Y.%m.%d.xml')
+
+if File.exists?(filename)
+  puts "Loading from #{filename}"
+else
+  puts "Not found: #{ARGV[0]}"
+  exit(1)
+end
+
+jobdoc = File.open(filename, 'r') { |f| Nokogiri::XML(f) }
 
 def self.de_entitize(txt)
   txt = txt.to_s
@@ -21,31 +30,39 @@ def self.de_entitize(txt)
   txt
 end
 
-jobdoc.css("job").each do |job|
-  title = job.css("title").text
-  guid = job.css("guid").text
-  category = job.css("job-category").text.strip
-  url = job.css("url").text
-  zip_code = job.css("zipcode").text.strip
-  jobcat = JobCat.data_for_category(category)
-  description = job.css("description").text
-  if boston_zip_codes.include?(zip_code) && jobcat && jobcat.training_lvl && jobcat.training_lvl <= 2
-    ev = Event.find_or_initialize_by jobcase_id: guid
-    ev.attributes = {
-      name: de_entitize(title),
-      description: de_entitize(description),
-      address: de_entitize(zip_code),
-      url: de_entitize(url)
-    }
-    if !ev.save
-      puts "Could not save jobcase job #{guid}"
-    elsif jobcat
-      jobcat.topics.each do |tp|
-        ev.topics << tp
-      end
-      zip_category = Topic.find_or_create_by! \
+Event.transaction do
+
+  # Anything we didn't update is deemed expired...
+
+  Event.where_current.update_all(expires_at: (Date.today - 1).end_of_day)
+
+  jobdoc.css("job").each do |job|
+    title = job.css("title").text
+    guid = job.css("guid").text
+    category = job.css("job-category").text.strip
+    url = job.css("url").text
+    zip_code = job.css("zipcode").text.strip
+    jobcat = JobCat.data_for_category(category)
+    description = job.css("description").text
+    if boston_zip_codes.include?(zip_code) && jobcat && jobcat.training_lvl && jobcat.training_lvl <= 2
+      ev = Event.find_or_initialize_by jobcase_id: guid
+      ev.attributes = {
+        name: de_entitize(title),
+        description: de_entitize(description),
+        address: de_entitize(zip_code),
+        url: de_entitize(url),
+        expires_at: (Date.today+14).end_of_day
+      }
+      if !ev.save
+        puts "Could not save jobcase job #{guid}"
+      elsif jobcat
+        jobcat.topics.each do |tp|
+          ev.topics << tp
+        end
+        zip_category = Topic.find_or_create_by! \
         name: zip_code, topic_group: 'zip_code'
-      ev.topics << zip_category
+        ev.topics << zip_category
+      end
     end
   end
 end
